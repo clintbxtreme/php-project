62fe4ffaa748537f84630e54e3ba17add9763f4f
Clinton Burnham <clinton.burnham@pluto.tv>
2022-10-10 12:59:23 -0700

    manage media watched state via a playlist

--- tvDelete.php
+++ tvDelete.php
@@ -7,19 +7,12 @@ require_once "tools.php";
 $tools = new Tools();
 $tools->curl_timeout = 60;
 
-$data_raw = $tools->callPlex("/library/sections/2/search?type=4", 'local');
+$data_raw = $tools->callPlex("/playlists/76044/items", 'local');
 $episodes = $data_raw['MediaContainer']['Metadata'];
 
 foreach ($episodes as $ep) {
     $show_title = $ep['grandparentTitle'];
-    if (in_array($show_title, $tools->config['shows_to_skip'])) {
-        continue;
-    }
     $episode_name = "{$show_title} - S{$ep['parentIndex']}E{$ep['index']}";
-    if (empty($ep['viewCount']) && in_array($show_title, $tools->config['shows_watching'])) {
-        // $tools->logToFile("Still need to watch {$episode_name}");
-        continue;
-    }
     foreach ($ep['Media'] as $filename_info) {
         $filename = $filename_info['Part'][0]['file'];
         if (!strpos($filename, 'Other Videos')) {
